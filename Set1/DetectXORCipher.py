ead2a4162c60fb770067f9000d3e29e3d6581712
Mason Silber <mds2161@columbia.edu>
2014-08-12 19:37:36 -0400

    Refactored existing code, changed scoring algorithm, and solved the fourth challenge

---
+++ Set1/DetectXORCipher.py
@@ -0,0 +1,17 @@
+import SingleByteXORCipher as XORCipher
+
+def find_cipher_in_list(cipher_list):
+	best_match_string = ''
+	best_match_score = 0
+	index = 0
+	for possible_cipher in cipher_list:
+		possible_best_match = XORCipher.decode(possible_cipher)[0]
+		if possible_best_match[1] > best_match_score:
+			best_match_score = possible_best_match[1]
+			best_match_string = str(possible_best_match[0])
+	
+	return (best_match_string, best_match_score)
+
+possible_ciphers = open('./4.txt', 'r').read().split()
+best_match = find_cipher_in_list(possible_ciphers)
+print str(best_match[0]).rstrip() + " : " + str(best_match[1])
--- Set1/SingleByteXORCipher.py
+++ Set1/SingleByteXORCipher.py
@@ -1,15 +1,5 @@
 import XOR
-
-words_list = open('/usr/share/dict/words', 'r').read().split()
-
-def score_string(string):
-	number_of_valid_characters = 0
-	for word in string.split():
-		if word in words_list:
-			number_of_valid_characters += len(word)
-
-	return float(number_of_valid_characters)/float(len(string))
-
+import ScoreString
 
 def decode(encoded_string):
 	possible_decryptions = list()
@@ -23,11 +13,9 @@ def decode(encoded_string):
 	scored_decryptions = list()
 	index = 1
 	for possible_decryption in possible_decryptions:
-		scored_decryptions.append((possible_decryption, score_string(possible_decryption)))
+		scored_decryptions.append((possible_decryption, ScoreString.score_string(possible_decryption)))
 
 	scored_decryptions.sort(key=lambda tuple: tuple[1], reverse=True)
-
-	print scored_decryptions[0][0]
-
-
-decode('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+	return scored_decryptions
+	
+#print decode('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')[0]
