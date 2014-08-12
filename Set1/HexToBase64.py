711def00f52ea4fbe14ba37a4e39c640f95390c0
Mason Silber <mason@twitter.com>
2014-08-12 13:54:48 -0400

    Renamed files

--- Set1/HexToBase64.py
+++
@@ -1,8 +0,0 @@
-import binascii
-
-def hex_to_base_64(hex_string):
-	binary_string = binascii.unhexlify(hex_string)
-	base_64_string = binascii.b2a_base64(binary_string)
-	return base_64_string
-
-#hex_to_base_64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
--- Set1/XOR.py
+++
@@ -1,4 +0,0 @@
-import binascii
-
-def XOR(hex_string_1, hex_string_2):
-	return str(hex(int(hex_string_1, 16) ^ int(hex_string_2, 16)))
